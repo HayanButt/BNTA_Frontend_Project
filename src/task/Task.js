40a59d05287716a6c5c616445d5b7bfceec8bd4a
Umar Twynam <umartwynam@gmail.com>
2023-05-30 18:01:43 +0100

    basic checkbox

--- src/task/Task.js
+++ src/task/Task.js
@@ -1,9 +1,22 @@
+import React, { useState } from 'react';
+
 const Task = ({task}) => {
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited);
+      };
+
+    
     return ( 
         <>
             <h3>{task.content}</h3>
             <p>{task.dueDate}</p>
             <p>{task.priority}</p>
+        <label>
+        Visited:
+        <input type="checkbox" checked={visited} onChange={handleVisited} />
+      </label>
         </>
      );
 }
