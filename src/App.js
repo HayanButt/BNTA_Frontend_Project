884ffcb613dc0df2a6caf0707c4e36d60d5ddd67
Taliane Moussassa <talianegloire392@gmail.com>
2023-05-30 15:13:43 +0100

    Shows animal tasks using the flat method in AnimalContainer

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ function App() {
   return (
     <>
       <UserContainer />
-      <TaskContainer />
       <FooterContainer />
     </>
 
--- src/animal/AnimalContainer.js
+++ src/animal/AnimalContainer.js
@@ -1,18 +1,28 @@
 import { useEffect, useState } from "react";
 import AnimalList from "./AnimalList";
+import TaskList from "../task/TaskList";
+
 
 const AnimalContainer = ({currentUser}) => {
     const [userAnimals, setUserAnimals] = useState([])
+    const [currentUserTaskList, setCurrentUserTaskList] = useState([])
 
     useEffect(()=> {
         if (currentUser.id) {
             setUserAnimals(currentUser.animals)
+            // loop through map 
+            const foundTasks = currentUser.animals.map((animal) => {
+                return animal.tasks
+            }).flat()
+            setCurrentUserTaskList(foundTasks);
         }
     }, [currentUser])
 
     return ( 
         <>
             <AnimalList userAnimals={userAnimals}/>
+            <TaskList currentUserTaskList={currentUserTaskList}/>
+            
         </>
      );
 }
