1048c3f917bc22509e8048087359e1b9450e733b
Kelly Wong <kellykl_wong@hotmail.com>
2023-05-31 12:50:17 +0100

    adds task options in task form

--- src/task/TaskForm.js
+++ src/task/TaskForm.js
@@ -1,14 +1,33 @@
 import {useState} from 'react'
+import Select from 'react-select'
 
 const TaskForm = ({saveTask, userAnimals}) => {
-
-
     const [newTask, setNewTask] = useState({
         content: "",
         dueDate: "",
         priority: "",
-        completed: false
+        completed: false,
+        taskTypeId: ""
     })
+   const [currentAnimal, setCurrentAnimal] = useState(null)
+
+    const taskOptions = [
+        {value: "LITTERBOX", label: "LITTERBOX", link: "Cat"},
+        {value: "FEEDING", label: "FEEDING", link: "Cat"},
+        {value: "GROOMING", label: "GROOMING", link: "Cat"},
+        {value: "MEDICATION", label: "MEDICATION", link: "Cat"},
+        {value: "WATER", label: "WATER", link: "Cat"},
+        {value: "PLAYTIME", label: "PLAYTIME", link: "Cat"},
+        {value: "VETERINARYCARE", label: "VETERINARYCARE", link: "Cat"},
+        {value: "MEDICATION", label: "MEDICATION", link: "Dog"},
+        {value: "FEEDING", label: "FEEDING", link: "Dog"},
+        {value: "GROOMING", label: "GROOMING", link: "Dog"},
+        {value: "WATER", label: "WATER", link: "Dog"},
+        {value: "PLAYTIME", label: "PLAYTIME", link: "Dog"},
+        {value: "VETERINARYCARE", label: "VETERINARYCARE", link: "Dog"},
+        {value: "MAINTENANCE", label: "MAINTENANCE", link: "Fish"},
+        {value: "FEEDING", label: "FEEDING", link: "Fish"}
+    ]
 
     const animalOptions = userAnimals.map((animal) => {
         return (
@@ -18,13 +37,15 @@ const TaskForm = ({saveTask, userAnimals}) => {
         ) 
     })
 
-    const animalTypeOptions = userAnimals.animalType[1].availableTask.map((task) => {
-        return (
-            <option key={task.id} value={task.id}>
-                {task}
-            </option>
-        )
-    })
+    
+
+    // const animalTypeOptions = userAnimals.animalType[1].availableTask.map((task) => {
+    //     return (
+    //         <option key={task.id} value={task.id}>
+    //             {task}
+    //         </option>
+    //     )
+    // })
     
     
     const handleAnimalChange = (event) => {
@@ -36,6 +57,7 @@ const TaskForm = ({saveTask, userAnimals}) => {
         const copiedTask = {... newTask}
         copiedTask.animal = selectedAnimal
         setNewTask(copiedTask)
+        setCurrentAnimal(selectedAnimal)
     }
 
     const handleChange =(event) => {
@@ -54,21 +76,8 @@ const TaskForm = ({saveTask, userAnimals}) => {
             priority: "",
             completed: false
         })
-      }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    }
+
     return ( 
         <form onSubmit={handleFormSubmit}>
             <input onChange={handleChange}
@@ -96,6 +105,8 @@ const TaskForm = ({saveTask, userAnimals}) => {
                 {animalOptions}
             </select>
 
+            <Select options={taskOptions.filter((task) => task.link === currentAnimal?.animalType.animalTypeName)}></Select>
+            {/* <Select options={currentAnimal?.animalType.availableTasks}></Select> */}
             <button type="submit">Submit</button>
         </form>
      );
