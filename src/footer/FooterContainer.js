e913dc0904a6637cef561d07f12d4d771832dbd0
Kelly Wong <kellykl_wong@hotmail.com>
2023-06-01 17:34:04 +0100

    adds divs in footer

--- src/footer/FooterContainer.js
+++ src/footer/FooterContainer.js
@@ -4,8 +4,13 @@ import './Footer.css';
 const FooterContainer = () => {
     return ( 
         <div className="footer-container">
+            <div className="footer-button-top-container">
+                <button className="footer-buttons" role="button">Animals</button>
+                <button className="footer-buttons" role="button">Tasks</button>
+                <button className="footer-buttons" role="button">Users</button>
+            </div>
             
-            <div className="footer-button-container">
+            <div className="footer-button-bottom-container">
                 <button className="footer-buttons" role="button">About Us</button>
                 <button className="footer-buttons" role="button">Socials</button>
                 <button className="footer-buttons" role="button">FAQs</button>
